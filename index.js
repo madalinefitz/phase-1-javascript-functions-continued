ef1f9f40d25ea5bb53584f8a320bb01e7cfb5f2a
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-01-31 22:51:30 -0500

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+function saturdayFun(activity){
+    if (activity === undefined) {
+    return (`This Saturday, I want to roller-skate!`)
+    }
+    if (activity !== undefined) {
+    return (`This Saturday, I want to ${activity}!`)
+    }
+} 
+
+function mondayWork(job){
+    if (job === undefined) {
+    return (`This Monday, I will go to the office.`)
+    }
+    if (job !== undefined) {
+    return (`This Monday, I will ${job}.`)
+    }
+}
+
+function wrapAdjective (symbol = "*") {
+    return function (adjective = "special"){
+        return (`You are ${symbol}${adjective}${symbol}!`)
+        }
+    }
+
+
+
